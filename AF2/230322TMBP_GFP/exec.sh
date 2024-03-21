4bd968378ee68e7d3f51e39fe3da7619ee39a00d
Burcin Acar <burcin.acar@crick.ac.uk>
2024-03-21 11:13:41 +0000

    Update exec.sh

--- AF2/230322TMBP_GFP/exec.sh
+++ AF2/230322TMBP_GFP/exec.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# This code gives order to start an array of AF2 calculations by running array.sh 
+# multiple times (number of fusionX folders - number of different fusion sequences).
+# Here the fusionX directories are located under oldAF2.
 num_folders=$(find oldAF2 -mindepth 1 -maxdepth 1 -type d | wc -l)
 sbatch --array=0-$num_folders array.sh 
